7f678f84f0d968c29590808301ffe1a827bc5fe3
AryanNateq <aryan.nateghnia-1@student.uts.edu.au>
2020-09-08 17:00:42 +1000

    Add greater form validation to  signUp.js

--- src/Components/SignUpFormContainer/SignUp.js
+++ src/Components/SignUpFormContainer/SignUp.js
@@ -9,7 +9,6 @@ function SignUp(props) {
   return (
     <div>
       <form
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
         onSubmit={(event) => {
           event.preventDefault();
           props.signUpUser(name, username, email, password);
@@ -19,41 +18,45 @@ function SignUp(props) {
           Create an account
         </h1>
 
-        <label className="input__label">Name</label>
+        <label className="form__label">Name</label>
         <input
           className="text-field focus:outline-none focus:bg-white"
           value={name}
           onChange={(event) => setName(event.target.value)}
+          required
         />
 
-        <label className="input__label">Username</label>
+        <label className="form__label">Username</label>
         <input
           className="text-field focus:outline-none focus:bg-white"
           value={username}
           onChange={(event) => setUsername(event.target.value)}
+          required
         />
 
-        <label className="input__label">Email Address</label>
+        <label className="form__label">Email Address</label>
         <input
           className="text-field focus:outline-none focus:bg-white"
           type="email"
           value={email}
           onChange={(event) => setEmail(event.target.value)}
+          required
         />
 
-        <label className="input__label">Password</label>
+        <label className="form__label">Password</label>
         <input
           className="text-field focus:outline-none focus:bg-white"
           type="password"
           value={password}
           onChange={(event) => setPassword(event.target.value)}
+          required
+          minLength="8"
+          pattern="^(?=.*[a-z])(?=.*[0-9])\S{8,99}$"
+          title="Password must contain letters, numbers and must be atleast 8 digits."
         />
 
         <div className="flex justify-center mt-6">
-          <button
-            className="align-center shadow center bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-            type="submit"
-          >
+          <button className="form__button form__button--primary" type="submit">
             Next
           </button>
         </div>
