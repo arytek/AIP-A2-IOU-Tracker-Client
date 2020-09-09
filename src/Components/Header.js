3047d036f4ee35eebb25a4ec4a104ac48de1c0d5
AryanNateq <aryan.nateghnia-1@student.uts.edu.au>
2020-09-09 13:29:59 +1000

    Update Header.js. Add login button to header.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,15 @@
-import React from "react";
-import Navigation from "./Navigation";
+import React from 'react';
+import Navigation from './Navigation';
+import LoginFormContainer from './LoginFormContainer/LoginFormContainer';
 
 function Header() {
   return (
     <header className="border-b p-3 flex justify-between items-center">
       <span className="font-bold">AppName</span>
-      <Navigation />
+      <div className="flex justify-between items-center">
+        <LoginFormContainer />
+        <Navigation />
+      </div>
     </header>
   );
 }
