2f4cc88b9010cd2ce73bc5b95b103ded7f4e709d
AryanNateq <aryan.nateghnia-1@student.uts.edu.au>
2020-09-09 13:31:38 +1000

    Update Navigation.js. Make use of single quotes instead of double quotes. Use overlay--black class in maskTransitions overlay.

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { useTransition, animated } from "react-spring";
-import NavigationMenu from "./NavigationMenu";
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useTransition, animated } from 'react-spring';
+import NavigationMenu from './NavigationMenu';
 
 function Navigation() {
   const [showMenu, setShowMenu] = useState(false);
 
   const maskTransitions = useTransition(showMenu, null, {
-    from: { position: "absolute", opacity: 0 },
+    from: { position: 'absolute', opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0 },
   });
 
   const menuTransitions = useTransition(showMenu, null, {
-    from: { opacity: 0, transform: "translateX(-100%)" },
-    enter: { opacity: 1, transform: "translateX(0%)" },
-    leave: { opacity: 0, transform: "translateX(-100%)" },
+    from: { opacity: 0, transform: 'translateX(-100%)' },
+    enter: { opacity: 1, transform: 'translateX(0%)' },
+    leave: { opacity: 0, transform: 'translateX(-100%)' },
   });
 
   return (
     <nav>
-      <span className="text-xl">
+      <span className="text-xl cursor-pointer">
         <FontAwesomeIcon icon={faBars} onClick={() => setShowMenu(!showMenu)} />
       </span>
 
@@ -31,7 +31,7 @@ function Navigation() {
             <animated.div
               key={key}
               style={props}
-              className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+              className="overlay--black"
               onClick={() => setShowMenu(false)}
             ></animated.div>
           )
@@ -43,7 +43,7 @@ function Navigation() {
             <animated.div
               key={key}
               style={props}
-              className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow p-3"
+              className="fixed bg-white top-0 left-0 w-1/4 h-full z-50 shadow p-3"
             >
               <NavigationMenu closeMenu={() => setShowMenu(false)} />
             </animated.div>
