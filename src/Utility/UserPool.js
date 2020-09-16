5cd6766c334ac79505ab3dee8732c231fe23d6e5
AryanNateq <aryan.nateghnia-1@student.uts.edu.au>
2020-09-16 14:31:52 +1000

    Update UserPool data to use 'favor-tracker-2' UserPool.

--- src/Utility/UserPool.js
+++ src/Utility/UserPool.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-  UserPoolId: "us-east-2_fBK9Ts1mh",
-  ClientId: "7r5v8hjqh68ldg1g1sd2e4240r",
+  UserPoolId: 'us-east-2_7jQgRzLnC',
+  ClientId: '23iev2bmn7d5gf88au24fh81jj',
 };
 
 export default new CognitoUserPool(poolData);
