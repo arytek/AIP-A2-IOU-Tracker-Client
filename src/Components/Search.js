0cc11bde87e3a28575bf2a15e7e19381fbcf6fe0
mohammad260 <53283097+mohammad260@users.noreply.github.com>
2020-10-20 20:10:00 +1100

    change search bar layout design

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,28 +1,39 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Search.css';
+ import { Link } from 'react-router-dom';
+ import "./Search.css";
 
-function Search() {
+ function Search() {
   const [search, setSearch] = useState('');
-
   return (
-    <div className="flex flex-row h-auto flex-start my-5">
-      <input
-        className="flex-auto border-gray-500 border rounded-lg mr-6 py-2 px-5 w-9/12"
+    <div>
+      <input 
         type="text"
         id="search"
-        placeholder="Search"
+        className="py-2 mb-3 border border-gray-900 rounded-lg px-2 w-64 padding-right-300"
+        placeholder="Keywords..."
         value={search}
         onChange={(event) => {
           setSearch(event.target.value);
         }}
       />
-      <Link
-        to={`/products/${search}`}
-        className="flex-auto border-gray-900 border rounded-lg py-2 px-5 cursor-pointer text-center"
+      <div className="dropdown">
+        <button className="border border-gray-900 py-2 text-left rounded-lg dropdown margin-left px-4">
+          Reward Type
+          <span className="px-4">
+            <i className="arrow down"></i>
+          </span>
+        </button>
+        <div className="dropdown-content">
+          <p>hello</p>
+        </div>
+      </div>
+      
+      <button
+        // className="text mx-5 border border-gray-900 rounded-lg px-5 cursor-pointer"
+        className="bg-blue-500 text-white py-2 justify-center rounded-lg margin-left px-6"
       >
         Search
-      </Link>
+      </button>
     </div>
   );
 }
