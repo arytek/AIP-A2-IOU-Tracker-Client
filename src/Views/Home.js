f7bf93a5dcad775dbecb42b7291064bdd67af88b
AryanNateq <aryan.nateghnia-1@student.uts.edu.au>
2020-11-01 22:32:34 +1100

    Update CSS in Home.js.

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -11,64 +11,64 @@ import Loader from '../Components/Loader';
  * TODO: Refactor so that it displays a list of requests.
  */
 function Home() {
-	//const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/requests/?page=1&limit=10`;
+  //const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/requests/?page=1&limit=10`;
 
-	let requests = requestsData;
-	let content = null;
-	const [currentPage, setCurrentPage] = useState(0);
-	const [data, setData] = useState([]);
-	/* set quantity of data in each page  */
-	const PER_PAGE = 10;
-	const offset = currentPage * PER_PAGE;
-	const pageCount = Math.ceil(data.length / PER_PAGE);
+  let requests = requestsData;
+  let content = null;
+  const [currentPage, setCurrentPage] = useState(0);
+  const [data, setData] = useState([]);
+  /* set quantity of data in each page  */
+  const PER_PAGE = 10;
+  const offset = currentPage * PER_PAGE;
+  const pageCount = Math.ceil(data.length / PER_PAGE);
 
-	useEffect(() => {
-		setData(requests);
-	}, []);
+  useEffect(() => {
+    setData(requests);
+  }, []);
 
-	/* handle for selecting page  */
-	function handlePageClick({ selected: selectedPage }) {
-		setCurrentPage(selectedPage);
-	}
+  /* handle for selecting page  */
+  function handlePageClick({ selected: selectedPage }) {
+    setCurrentPage(selectedPage);
+  }
 
-	if (requests.error) {
-		content = <p>There was an error please refresh or try again later.</p>;
-	}
+  if (requests.error) {
+    content = <p>There was an error please refresh or try again later.</p>;
+  }
 
-	if (requests.loading) {
-		content = <Loader></Loader>;
-	}
+  if (requests.loading) {
+    content = <Loader></Loader>;
+  }
 
-	if (requests) {
-		/* handle for display request data */
-		content = data.slice(offset, offset + PER_PAGE).map((request) => (
-			<div key={request.id}>
-				<RequestCard request={request} />
-			</div>
-		));
-	}
+  if (requests) {
+    /* handle for display request data */
+    content = data.slice(offset, offset + PER_PAGE).map((request) => (
+      <div key={request.id}>
+        <RequestCard request={request} />
+      </div>
+    ));
+  }
 
-	return (
-		<div>
-			<div>
-				<h1 className='text-2xl mb-3'>Requests</h1>
-				<Search />
-				{content}
-				<div className='flex flex-wrap justify-center text-center font-bold text-blue-500 cursor-pointer '>
-					<ReactPaginate
-						previousLabel={'← Previous'}
-						nextLabel={'Next →'}
-						pageCount={pageCount}
-						onPageChange={handlePageClick}
-						containerClassName={'pagination'}
-						previousLinkClassName={'pagination__link'}
-						nextLinkClassName={'pagination__link'}
-						disabledClassName={'pagination__link--disabled'}
-						activeClassName={'pagination__link--active'}
-					/>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="text-center m-6 mx-auto w-full max-w-2xl">
+      <div className="">
+        <h1 className="font-sans text-2xl text-left mb-4">Requests</h1>
+        <Search />
+        {content}
+        <div className="flex flex-wrap justify-center text-center font-bold text-blue-500 cursor-pointer ">
+          <ReactPaginate
+            previousLabel={'← Previous'}
+            nextLabel={'Next →'}
+            pageCount={pageCount}
+            onPageChange={handlePageClick}
+            containerClassName={'pagination'}
+            previousLinkClassName={'pagination__link'}
+            nextLinkClassName={'pagination__link'}
+            disabledClassName={'pagination__link--disabled'}
+            activeClassName={'pagination__link--active'}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Home;
